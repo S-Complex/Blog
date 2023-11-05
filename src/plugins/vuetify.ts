ff5e5753367db918c3c14db80d04a00c78a80d68
SliverRiver327 <Restent@tutanota.com>
2023-11-05 21:20:32 +0800

    Change theme colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,8 +11,8 @@ export default defineNuxtPlugin((nuxtApp) => {
           colors: {
             background: "#ECEFF1",
             surface: "#FFFFFF",
-            primary: "#1e90ff",
-            secondary: "#87CEFA",
+            primary: "#2196F3",
+            secondary: "#03A9F4",
             blockquote: "#ECEFF1",
             blockquoteBorder: "#039BE5"
           },
@@ -21,8 +21,8 @@ export default defineNuxtPlugin((nuxtApp) => {
           colors: {
             background: "#212121",
             surface: "#263238",
-            primary: "#1e90ff",
-            secondary: "#87CEFA",
+            primary: "#2196F3",
+            secondary: "#03A9F4",
             blockquote: "#455A64",
             blockquoteBorder: "#0277BD"
           },
