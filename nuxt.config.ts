62863607535e25b628413b8b208acd794e8f5dd2
SliverRiver327 <Restent@tutanota.com>
2023-11-09 19:12:37 +0800

    Add RSS & Sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,20 @@ export default defineNuxtConfig({
   },
   modules: [
     '@nuxt/content',
+    'nuxt-simple-sitemap',
+    'nuxt-feedme',
     async (options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
         vuetify()
       ))
     },
   ],
+  site: {
+    url: 'https://blog.restent.win',
+  },
+  sitemap: {
+    autoLastmod: false,
+  },
   content: {
     highlight: {
       theme: "material-theme-ocean",
