7411886f67bb1e672e89c9f461f48715c5bab803
SliverRiver327 <Restent@tutanota.com>
2023-12-29 20:55:13 +0800

    Rewrite style of pagination

--- .vitepress/theme/components/page/Pagintaion.vue
+++ .vitepress/theme/components/page/Pagintaion.vue
@@ -2,6 +2,7 @@
 import { useData } from 'vitepress'
 import { data as posts } from './posts.data';
 import { computed } from 'vue';
+import { mdiArrowLeft, mdiArrowRight } from '@mdi/js'
 
 const PAGE_SIZE = 10
 const { params } = useData()
@@ -10,16 +11,16 @@ const currentPage = computed(() => (params.value?.page || 1) as number)
 </script>
 
 <template>
-<div class="d-flex justify-center">
-  <div class="pa-2 ma-2">
-    <v-btn v-if="currentPage === 2" :href="`/`">上一页</v-btn>
-    <v-btn v-else :disabled="currentPage === 1" :href="`/page/${currentPage - 1}`">上一页</v-btn>
+  <div class="d-flex justify-space-between mb-6">
+    <div class="pa-2 ma-2">
+      <v-btn icon v-if="currentPage === 2" :href="`/`"><v-icon :icon="mdiArrowLeft" /></v-btn>
+      <v-btn icon v-else :disabled="currentPage === 1" :href="`/page/${currentPage - 1}`"><v-icon :icon="mdiArrowLeft" /></v-btn>
+    </div>
+    <div class="pa-1 ma-1 align-self-center">
+      <p class="text-body-2">第 {{ currentPage }} 页，共 {{ totalPages }} 页</p>
+    </div>
+    <div class="pa-2 ma-2">
+      <v-btn icon :disabled="currentPage === totalPages" :href="`/page/${currentPage + 1}`"><v-icon :icon="mdiArrowRight" /></v-btn>
+    </div>
   </div>
-  <div class="pa-2 ma-2 align-self-center">
-    <p>第 {{ currentPage }} 页</p>
-  </div>
-  <div class="pa-2 ma-2">
-    <v-btn :disabled="currentPage === totalPages" :href="`/page/${currentPage + 1}`">下一页</v-btn>
-  </div>
-</div>
 </template>
